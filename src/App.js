582cae18e4288dfe1d40484c8e86006d83c83e27
Ozge <erolozge01@gmail.com>
2022-07-12 16:08:59 -0500

    firebase authentication

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import { useState } from "react";
+import FirebaseAuthService from "./FirebaseAuthService";
+
+import LoginForm from "./components/LoginForm";
 
 function App() {
+  const [user, setUser] = useState(null);
+  FirebaseAuthService.subscribeToAuthChanges(setUser);
   return (
     <div className="App">
-      <h1>Firebase Web App</h1>
-      <h2>Recipes</h2>
+      <div className="title-row">
+        <h1 className="title"> Recipes</h1>
+        <LoginForm existingUser={user} />
+      </div>
     </div>
   );
 }
